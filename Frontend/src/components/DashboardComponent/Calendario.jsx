2b55b9c1b75f7a229ba7981cc0fdd491ab994a4b
manuel00085 <manuel00085@gmail.com>
2024-09-05 15:58:18 -0400

    Actualizacion formulario

--- Frontend/src/components/DashboardComponent/Calendario.jsx
+++ Frontend/src/components/DashboardComponent/Calendario.jsx
@@ -11,12 +11,21 @@ const Calendario = () => {
 
   const localizer = dayjsLocalizer(dayjs);
 
-
   const [modalOpen, setModalOpen] = useState(false);
-  const [eventData, setEventData] = useState({  Name: '', time: '', Subname: '', start: '', Sex: '', Phone: '', Specialty: '' });
+  const [eventData, setEventData] = useState({
+
+    Name: '',
+    time: '',
+    Subname: '',
+    start: '',
+    Sex: '',
+    Phone: '',
+    Specialty: '',
+    SpecialtyDr: '',
+    Cedula: ''
+  });
   const [selectedDate, setSelectedDate] = useState(null); // Estado para el día seleccionado
 
-  
   const handleSelectSlot = (slotInfo) => {
     const dayOfWeek = dayjs(slotInfo.start).day();
     // Evitar abrir el modal en sábados (6) y domingos (0)
@@ -30,7 +39,6 @@ const Calendario = () => {
       setModalOpen(true); // Abrir modal cuando se hace clic en un día
     }
   };
-
 
   const handleEventChange = (e) => {
     setEventData({ ...eventData, [e.target.name]: e.target.value });
@@ -65,7 +73,7 @@ const Calendario = () => {
     // Deshabilitar sábado y domingo
     if (day === 0 || day === 6) {
       return {
-        className: 'bg-gray-200 text-gray-400 cursor-not-allowed',
+        className: 'bg-gray-200 bg-opacity-50 text-gray-400 cursor-not-allowed ',
         style: {
           pointerEvents: 'none',
         },
@@ -75,53 +83,49 @@ const Calendario = () => {
     // Aplicar estilo especial si el día está seleccionado o en hover
     if (isSameDate) {
       return {
-        className: 'bg-blue-100 text-blue-900',
+        className: 'bg-blue-100 bg-opacity-50 text-blue-900',
       };
     }
 
     // Aplicar efecto de hover
     return {
-      className: 'hover:bg-blue-200 cursor-pointer',
+      className: 'hover:bg-blue-200 bg-opacity-50   cursor-pointer ',
     };
   };
 
   return (
     <div className='flex justify-center font-medium w-full content-center h-full'>
+      <Calendar
+        selectable={true}
+        localizer={localizer}
+        events={[]} // Aquí colocarías tus eventos
+        onSelectSlot={handleSelectSlot} // Usar la función handleSelectSlot
+        onNavigate={handleNavigate} // Agregar la función para manejar la navegación
+        onDrillDown={handleDrillDown} // Evitar drill down a vista de día o semana
+        dayPropGetter={dayStyleGetter} // Añadir función para estilos de días
+        max={dayjs('2050-01-01T18:00:00').toDate()}
+        min={dayjs('2000-01-01T08:00:00').toDate()}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: '80vh', width: '100%', padding: '7px', border: '1px solid #ccc' }}
+        messages={{
+          next: 'Siguiente',
+          previous: 'Anterior',
+          today: 'Hoy',
+          month: 'Mes',
+          week: 'Semana',
+          day: 'Día',
+        }}
+        className="bg-white rounded-md shadow-2xl"
+      />
 
-  
-        <Calendar
-          selectable={true}
-          localizer={localizer}
-          events={[]} // Aquí colocarías tus eventos
-          onSelectSlot={handleSelectSlot} // Usar la función handleSelectSlot
-          onNavigate={handleNavigate} // Agregar la función para manejar la navegación
-          onDrillDown={handleDrillDown} // Evitar drill down a vista de día o semana
-          dayPropGetter={dayStyleGetter} // Añadir función para estilos de días
-          max={dayjs('2050-01-01T18:00:00').toDate()}
-          min={dayjs('2000-01-01T08:00:00').toDate()}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: '80vh', width: '100%', padding: '7px',  border: '1px solid #ccc',  }}
-          messages={{
-            next: 'Siguiente',
-            previous: 'Anterior',
-            today: 'Hoy',
-            month: 'Mes',
-            week: 'Semana',
-            day: 'Día',
-
-          }}
-          className="bg-white rounded-md  shadow-2xl "
-        />
-
-        {modalOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-1/2 z-60">
-              <h2 className="text-xl font-bold mb-4">Agendar Cita</h2>
-              <form onSubmit={handleFormSubmit}>
-
-                <div className="mb-5 flex justify-between	">
-                  <div>
+      {modalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-1/2 z-60">
+            <h2 className="text-xl font-bold mb-4">Agendar Cita</h2>
+            <form onSubmit={handleFormSubmit}>
+              <div className="mb-5 flex justify-between">
+                <div>
                   <label className=" text-gray-700 font-semibold mb-2">Nombres:</label>
                   <input
                     type="text"
@@ -131,9 +135,9 @@ const Calendario = () => {
                     required
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
-                  </div>
+                </div>
 
-                  <div>
+                <div>
                   <label className="text-gray-700 font-semibold mb-2">Apellidos:</label>
                   <input
                     type="text"
@@ -143,103 +147,128 @@ const Calendario = () => {
                     required
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
-
-                  </div>
-         
                 </div>
+              </div>
 
-                <div className="mb-5 flex justify-between	">
-                  <div>
-                  <label className=" text-gray-700 font-semibold mb-2">Genero:</label>
-                 <select 
-                    name="Sex" 
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                      <option value={eventData.Sex="Masculino"}>Masculino</option>
-                      <option value={eventData.Sex="Femenino"}>Femenino</option>
-
-                 </select>
-                  </div>
-
-                  <div>
-                  <label className="text-gray-700 font-semibold mb-2">Numero de Telefono:</label>
+              <div className="mb-5 flex justify-between">
+               
+               <div>
+                  <label className="text-gray-700 font-semibold mb-2">Numero de Cedula:</label>
                   <input
-                    type="phone"
-                    name="Phone"
-                    value={eventData.Phone}
+                    type="text"
+                    name="Cedula"
+                    value={eventData.Cedula}
                     onChange={handleEventChange}
                     required
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
-
-                  </div>
-         
                 </div>
 
-                <div className='mb-5'>
-                  <label className=" text-gray-700 font-semibold mb-2">Especialidad de la cita:</label>
-                 <select 
-                    name="Specialty" 
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                      <option value={eventData.Specialty="Psiquiatra"}>Psiquiatra</option>
-                      <option value={eventData.Specialty="Psicologo Clinico"}>Psicologo Clinico</option>
-                      <option value={eventData.Specialty="Psicologo Industrial"}>Psicologo Industrial</option>
-                      <option value={eventData.Specialty="Medico Internista"}>Medico Internista</option>
-                      <option value={eventData.Specialty="Medico Cirujano Ecografista"}>Medico Cirujano Ecografista</option>
-                      <option value={eventData.Specialty="Foniatria"}>Foniatria</option>
-                      <option value={eventData.Specialty="Terapeuta del Lenguaje"}>Terapeuta del Lenguaje</option>
-                      <option value={eventData.Specialty="Enfermera"}>Enfermera</option>
-                      <option value={eventData.Specialty="Traumatologia"}>Traumatologia</option>
-                      <option value={eventData.Specialty="Fisiatra"}>Fisiatra</option>
-                      <option value={eventData.Specialty="Fisioterapeuta"}>Fisioterapeuta</option>
-                      <option value={eventData.Specialty="Terapeuta Ocupacional"}>Terapeuta Ocupacional</option>
-                     
-
-                 </select>
-                  </div>
-                
-                <div className="mb-4">
-                  <label className="block text-gray-700 font-semibold mb-2">Fecha de Cita:</label>
-                  <input
-                    type="text"
-                    name="start"
-                    value={eventData.start}
-                    readOnly
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-700 font-semibold mb-2">Hora:</label>
-                  <input
-                    type="time"
-                    name="time"
-                    value={eventData.time}
+                <div>
+                  <label className=" text-gray-700 font-semibold mb-2">Genero:</label>
+                  <select
+                    name="Sex"
                     onChange={handleEventChange}
+                    value={eventData.Sex}
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                </div>
-                <div className="flex justify-end">
-                  <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">
-                    Agregar Evento
-                  </button>
-                  <button
-                    type="button"
-                    className="bg-red-500 text-white px-4 py-2 rounded-md mr-2"
-                    onClick={() => {
-                      setModalOpen(false);
-                      setSelectedDate(null); // Restablecer el día seleccionado al cerrar el modal
-                    }}
-                   
                   >
-                    Cancelar
-                  </button>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Femenino">Femenino</option>
+
+                  </select>
                 </div>
-              </form>
-            </div>
+
+
+              </div>
+
+              <div className="mb-5">
+                <label className="text-gray-700 font-semibold mb-2 block">Especialidad de la cita:</label>
+                <select
+                  name="Specialty"
+                  value={eventData.Specialty}
+                  onChange={handleEventChange}
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white shadow-md transition duration-150 ease-in-out"
+                >
+                  <option value="" disabled>Selecciona una especialidad</option>
+                  <option value="Psiquiatra">Psiquiatra</option>
+                  <option value="Psicologo Clinico">Psicólogo Clínico</option>
+                  <option value="Psicologo Industrial">Psicólogo Industrial</option>
+                  <option value="Medico Internista">Médico Internista</option>
+                  <option value="Medico Cirujano Ecografista">Médico Cirujano Ecografista</option>
+                  <option value="Foniatria">Foniatría</option>
+                  <option value="Terapeuta del Lenguaje">Terapeuta del Lenguaje</option>
+                  <option value="Enfermera">Enfermera</option>
+                  <option value="Traumatologia">Traumatología</option>
+                  <option value="Fisiatra">Fisiatra</option>
+                  <option value="Fisioterapeuta">Fisioterapeuta</option>
+                  <option value="Terapeuta Ocupacional">Terapeuta Ocupacional</option>
+                </select>
+              </div>
+
+              <div className="mb-5">
+                <label className="text-gray-700 font-semibold mb-2 block">Licenciado / Doctor / Especialista:</label>
+                <select
+                  name="SpecialtyDr"
+                  value={eventData.SpecialtyDr}
+                  onChange={handleEventChange}
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md bg-white shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                >
+                  <option value="" disabled>Selecciona un especialista</option>
+                  <option value="Dra. Alba Montenegro">Dra. Alba Montenegro</option>
+                  <option value="Dr. Jose Cedre">Dr. Jose Cedre</option>
+                  <option value="Dra. Naiyethsy Urbina">Dra. Naiyethsy Urbina</option>
+                  <option value="Dra. Alexis Barazarte">Dra. Alexis Barazarte</option>
+                  <option value="Dra. Milena Lopez">Dra. Milena Lopez</option>
+                  <option value="Dra. Susana Perez">Dra. Susana Perez</option>
+                  <option value="Lic. Lisbeth Nairoby Jerez Mata">Lic. Lisbeth Nairoby Jerez Mata</option>
+                  <option value="Dra. Arline Hernandez">Dra. Arline Hernandez</option>
+                  <option value="Lic. Clayde Sabina Key Gamero">Lic. Clayde Sabina Key Gamero</option>
+                  <option value="Dra. Adriana Perez">Dra. Adriana Perez</option>
+                  <option value="Dr. Luis Bustamante">Dr. Luis Bustamante</option>
+                  <option value="Dra. Nohelismar Camacho">Dra. Nohelismar Camacho</option>
+                </select>
+              </div>
+
+
+              <div className="mb-4">
+                <label className="block text-gray-700 font-semibold mb-2">Fecha de Cita:</label>
+                <input
+                  type="text"
+                  name="start"
+                  value={eventData.start}
+                  readOnly
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-semibold mb-2">Hora:</label>
+                <input
+                  type="time"
+                  name="time"
+                  value={eventData.time}
+                  onChange={handleEventChange}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div className="flex justify-end">
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">
+                  Agregar Evento
+                </button>
+                <button
+                  type="button"
+                  className="bg-red-500 text-white px-4 py-2 rounded-md mr-2"
+                  onClick={() => {
+                    setModalOpen(false);
+                    setSelectedDate(null); // Restablecer el día seleccionado al cerrar el modal
+                  }}
+                >
+                  Cancelar
+                </button>
+              </div>
+            </form>
           </div>
-        )}
-   
+        </div>
+      )}
     </div>
   );
 };
