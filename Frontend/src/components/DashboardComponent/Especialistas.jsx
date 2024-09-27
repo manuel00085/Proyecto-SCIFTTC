e427216a79979e4c7aee8c15729f8e778960f032
manuel00085 <manuel00085@gmail.com>
2024-09-27 16:01:59 -0400

    cambios doctores

--- Frontend/src/components/DashboardComponent/Especialistas.jsx
+++ Frontend/src/components/DashboardComponent/Especialistas.jsx
@@ -1,10 +1,36 @@
-const Especialistas = () => {
-    return (
-        <h1>Especialistas</h1>
-       
+import React from 'react';
 
+const specialists = [
+  { name: 'Dra. Alba Montenegro', specialty: 'Psiquiatra' },
+  { name: 'Dr. Jose Cedre', specialty: 'Medico Cirujano' },
+  { name: 'Dra. Naiyethsy Urbina', specialty: 'Psicólogo Clínico' },
+  { name: 'Dra. Alexis Barazarte', specialty: 'Fisioterapeuta' },
+  { name: 'Dra. Milena Lopez', specialty: 'Medico Internista' },
+  { name: 'Dra. Susana Perez', specialty: 'Psicólogo Industrial' },
+  { name: 'Lic. Lisbeth Nairoby Jerez Mata', specialty: 'Enfermera' },
+  { name: 'Dra. Arline Hernandez', specialty: 'Traumatología' },
+  { name: 'Lic. Clayde Sabina Key Gamero', specialty: 'Foniatría' },
+  { name: 'Dra. Adriana Perez', specialty: 'Fisiatra' },
+  { name: 'Dr. Luis Bustamante', specialty: 'Terapeuta del Lenguaje' },
+  { name: 'Dra. Nohelismar Camacho', specialty: 'Terapeuta Ocupacional' },
+];
 
-    );
-  };
-  
-  export default Especialistas;
+const SpecialistList = () => {
+  return (
+    <div className="max-w-screen-2xl  mx-auto bg-white shadow-md rounded-lg p-6">
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">Lista de Especialistas</h2>
+      <ul className="divide-y divide-gray-200">
+        {specialists.map((specialist, index) => (
+          <li key={index} className="py-3 flex justify-between items-center">
+            <div>
+              <p className="text-gray-900 font-semibold">{specialist.name}</p>
+              <p className="text-gray-500 text-sm">{specialist.specialty}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SpecialistList;
--- Frontend/src/components/DashboardComponent/Home.jsx
+++ Frontend/src/components/DashboardComponent/Home.jsx
@@ -4,7 +4,19 @@ import CardTitle from "./Card-title";
 
 const Inicio = () => {
    return(
-    <CardTitle Title={"Citas Aprobadas"} Text={"5% desde la semana pasada"}/>
+      <>
+      <div className="flex flex-wrap gap-8">
+
+      <CardTitle Title={"Citas para hoy"} Text={"5% desde la semana pasada"} Number={"8"}/>
+      <CardTitle Title={"Citas Atendidas"} Text={"5% desde la semana pasada"} Number={"3"}/>
+      <CardTitle Title={"Citas por Atender"} Text={"5% desde la semana pasada"} Number={"5"}/>
+      <CardTitle Title={"Citas para hoy"} Text={"5% desde la semana pasada"} Number={"55"}/>
+      <CardTitle Title={"Citas para hoy"} Text={"5% desde la semana pasada"} Number={"55"}/>
+      <CardTitle Title={"Citas para hoy"} Text={"5% desde la semana pasada"} Number={"55"}/>
+      </div>
+
+      </>
+
    )
 }
        
